267e0183f1a299d4b28f5275a0ede61658ab2ecc
Yimmy Eman <yimmy.eman@gmail.com>
2022-05-18 20:40:22 -0400

    update

--- datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template_new_version.py
+++ datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template_new_version.py
@@ -20,11 +20,20 @@ dataset = pd.read_csv("Data.csv")
 X = dataset.iloc[:, :-1].values 
 y = dataset.iloc[:, 3].values 
 
-# Tratamiento de los NAs
+# Tratamiento de los nan
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values = np.nan, strategy = "mean", verbose=0)
 imputer = imputer.fit(X[:,1:3]) 
 X[:, 1:3] = imputer.transform(X[:,1:3])
+
+'''
+# Otra forma interesante de tratar los nan
+# Sustituir
+z = dataset.fillna({"Age": 10, "Salary": 111111})
+
+# Eliminar
+w = dataset.dropna()
+'''
 
 # Codificar datos categoricos
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -46,9 +55,10 @@ y = labelencoder_y.fit_transform(y)
 # Dividir el data set en conjunto de entrenamiento y en conjunto de testing
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y,test_size = 0.2, random_state = 0)
-
+'''
 # Escalado de variables
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
+'''
